2430f2efc5e49ab8a98b862ef0860b168740cbc9
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-04-13 16:59:58 -0300

    feat: kata done

---
+++ kyu_8/Aquare(n)n Sum.py
@@ -0,0 +1,8 @@
+def square_sum(numbers):
+    """Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 12+22+22=91^2 + 2^2 + 2^2 = 912+22+22=9."""
+    return sum([num ** 2 for num in numbers])
+
+print(square_sum([1, 2, 2]))
+
